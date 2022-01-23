4784a1430a316c8ed79b1b8599cbb18c358fe379
Olena Chorna <olena.chorna@aptiv.com>
2022-01-23 20:15:17 +0100

    Chatbot <-> slack communication's set

---
+++ chatbot.py
@@ -0,0 +1,40 @@
+from http import client
+from urllib import response
+from flask import Flask, request
+from slackeventsapi import SlackEventAdapter
+import slack
+import requests
+from tokens import VERIFICATION_TOKEN, TOKEN
+
+app = Flask(__name__)
+# @app.route("/slack/events",methods=['GET','POST'])
+# def authorize():
+#   output = request.get_json()
+#   return output["challenge"]
+
+slack_events_adapter = SlackEventAdapter(VERIFICATION_TOKEN, 
+                                         "/slack/events", app)
+
+client = slack.WebClient(token=TOKEN)
+bot = client.api_call("auth.test")['user_id']
+
+@slack_events_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+    response = requests.post(
+          'http://localhost:5005/webhooks/rest/webhook',
+          json={"sender": user_id, "message": text})
+    response_text = response.json()
+    if user_id != bot:
+      for r_text in response_text:
+        client.chat_postMessage(channel="D0307SZ3S7J", text=r_text['text'])   
+
+
+
+
+if __name__ == "__main__":
+  app.run(port=3000, debug=False)
