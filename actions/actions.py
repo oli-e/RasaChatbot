aaf8f677209dd179bdf02d9f967afb63e7bbe4ab
Olena Chorna <olena.chorna@aptiv.com>
2022-01-26 23:53:24 +0100

    Added ActionAddress; ActionSummarizeOrder some improvements wishes (with/without/extra); addresses examples added to nlu.yml

--- actions/actions.py
+++ actions/actions.py
@@ -13,7 +13,24 @@ import json
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from datetime import date
+from math import floor
 import calendar
+import re
+
+
+class ActionAddress(Action):
+
+    def name(self) -> Text:
+        return "action_address"
+
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        msg = tracker.get_slot('address')
+        x = re.search("[a-zA-Z]+\s([0-9]+/[0-9]+|[0-9]+)", msg)
+        print(x.group())
+        dispatcher.utter_message(
+            text=f"Your order will be delivered to : {x.group()}\Thank you for your order!")
+
+        return []
 
 
 class ActionSummarizeOrder(Action):
@@ -25,26 +42,75 @@ class ActionSummarizeOrder(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        ingredient, order_str = "", ""
+        total, time, costs, preparation_time = 0, 0, 0, 0
+
+        for blob in tracker.latest_message['entities']:
+            if blob['entity'] == 'ingredient':
+                ingredient = blob['value']
+
+        numbers = '1 2 3 4 5 6 7 8 9 0'.split(' ')
         order = dict()
-        menu_items = self.__menu_list()
+        menu_items = self.__menu_list('name')
         msg = tracker.get_slot('order').lower().split(' ')
-        print(msg)
-        print(menu_items)
+        ingred_idx = 0
+        if ingredient in msg:
+            ingred_idx = msg.index(ingredient)
+        msg_single = [
+            w[:-1] if w.endswith('s') or w.endswith('.') or w.endswith(',') else w for w in msg]
+
         default = 1
-        for word in msg:
+        for word in msg_single:
+            item = dict()
             if word in menu_items:
-                order[word] = default
+                index = msg_single.index(word)
+                if index != 0:
+                    if msg_single[index - 1] in numbers:
+                        default = int(msg_single[index - 1])
+                name = msg[index]
+                if ingred_idx - index > 0:
+                    name = " ".join(
+                        [msg[index], msg[ingred_idx-1], ingredient])
+                order[word] = {'name': name,
+                               'amount': default}
 
-        print(order)
-
-        dispatcher.utter_message(text="Hello World!")
+        for key in order:
+            order_str += f"{order[key]['amount']} {order[key]['name']}, "
+            for item in self.menu['items']:
+                if item["name"].lower() == key:
+                    time = item["preparation_time"]
+                    costs = item["price"]
+            preparation_time += time * order[key]['amount']
+            total += costs * order[key]['amount']
+        time_str = self.__stringifyTime(preparation_time)
+        dispatcher.utter_message(
+            text=f"Let me summarize :D\nYour order is: {order_str[:-2]}\nIt'll take aproximatelly {time_str}\nTotal sums up to {total}$\nDo you want takeaway or delivery?")
 
         return []
 
-    def __menu_list(self):
+    def __stringifyTime(self, time: int) -> str:
+        hours = floor(time)
+
+        minutes = int((time - int(hours)) * 60)
+
+        if hours == 0:
+            return f"{minutes} minutes"
+        elif hours != 0 and minutes == 0:
+            if hours > 1:
+                return f"{hours} hours"
+            else:
+                return f"{hours} hour"
+        else:
+            if hours > 1:
+                return f"{hours} hours and {minutes} minutes"
+            else:
+                return f"{hours} hour and {minutes} minutes"
+
+    def __menu_list(self, name: str):
         with open('menu.json') as file:
             menu = json.load(file)
-        return [item['name'].lower() for item in menu['items']]
+        self.menu = menu
+        return [item[name].lower() for item in menu['items']]
 
 
 class ActionConfirmOpen(Action):
@@ -60,7 +126,6 @@ class ActionConfirmOpen(Action):
         opening_json = opening_time.get('items')
 
         for blob in tracker.latest_message['entities']:
-            print(blob)
             if blob['entity'] == 'day':
                 day = blob['value']
 
